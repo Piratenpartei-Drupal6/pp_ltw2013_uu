8ba74d2a52ebe803d2f9e1a749a193d5b788bd53
nowrap <nowrap@gmx.net>
2013-05-12 23:52:05 +0200

    minor fixes

--- pp_ltw2013_uu.js
+++ pp_ltw2013_uu.js
@@ -1,10 +1,10 @@
 function ltw2013_show(arr) {
-	console.log(arr);
+	//console.log(arr);
 	 $.ajax({
             url: '/ltw2013_uu_json.php',
 			dataType: 'json',
             success: function (data) {
-				console.log(data.data);
+				//console.log(data.data);
 				var content = '<ul class="menu">';
 				if (jQuery.inArray("ls", arr) != -1) {
 					content += '<li><a href="'+data.data.liste.uu+'">'+data.data.liste.title+'</a>'+ltw2013_sammelbalken(data.data.liste.info)+'</li>';
@@ -29,12 +29,11 @@ function ltw2013_show(arr) {
 }
 
 function ltw2013_sammelbalken(info) {
-	console.log(info);
+	//console.log(info);
 	var p_save = Math.round(info.save / info.max * 10000)/100;
 	var p_current = Math.round(info.current / info.max * 10000)/100;
 	var p_current_show = p_current - p_save;
 	var p_remain = 100 - p_current;
-	
 
 	var balken = '<table cellspacing="0" style="width: 100%; border: 1px solid black; background: #fff; padding: 0; margin: 0;">';
 	balken += '<tbody><tr style="height: 10px; line-height: 10px; font-size: 8px; text-align: center;">';
@@ -42,8 +41,13 @@ function ltw2013_sammelbalken(info) {
 		balken += '<td style="display: none; background-color: #fa0; padding: 0;">0&nbsp;%</td>';
 	else
 		balken += '<td style="width: '+p_save+'%; background-color: #ffaa00; padding: 0;">'+p_save+'&nbsp;%</td>';
-	balken += '<td style="width: '+p_current_show+'%; background-color: #fd3; padding: 0;">'+p_current+'&nbsp;%</td>';
-	balken += '<td style="width: '+p_remain+'%; background-color: transparent; padding: 0;"></td>';
+
+	if (p_current == 0) {
+		balken += '<td style="width: 100%; background-color: transparent; padding: 0;">0&nbsp;%</td>';
+	} else {
+		balken += '<td style="width: '+p_current_show+'%; background-color: #fd3; padding: 0;">'+p_current+'&nbsp;%</td>';
+		balken += '<td style="width: '+p_remain+'%; background-color: transparent; padding: 0;"></td>';
+	}
 	balken += '</tr></tbody></table>';
 
 	balken += '<table style="width: 100%; padding: 0; margin: 0;">';
